b81c8d961b6c41838f1cd45629d4646133e35acf
kitakoj18 <kitakoj18@gmail.com>
2018-01-24 12:37:43 -0700

    added script

---
+++ src/analysis.py
@@ -0,0 +1,74 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.feature_extraction import text
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.snowball import SnowballStemmer
+
+winedf = pd.read_csv('../input/winemag-data_first150k.csv')
+winedf.head()
+
+winedf.info()
+
+winedf[winedf['description'].duplicated(keep = False)].sort_values('description').head(8)
+winedf = winedf.drop_duplicates('description')
+
+variety_df = winedf.groupby('variety').filter(lambda x: len(x) > 1500)
+varieties = variety_df['variety'].value_counts().index.tolist()
+fig, ax = plt.subplots(figsize = (25, 10))
+sns.countplot(x = variety_df['variety'], order = varieties, ax = ax)
+plt.xticks(rotation = 90)
+plt.show()
+
+print(variety_df['variety'].unique())
+
+punc = ['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}',"%"]
+stop_words = text.ENGLISH_STOP_WORDS.union(punc)
+desc = variety_df['description'].values
+vectorizer = TfidfVectorizer(stop_words = stop_words)
+X = vectorizer.fit_transform(desc)
+
+word_features = vectorizer.get_feature_names()
+word_features[550:575]
+
+stemmer = SnowballStemmer('english')
+tokenizer = RegexpTokenizer(r'[a-zA-Z\']+')
+
+def tokenize(text):
+    return [stemmer.stem(word) for word in tokenizer.tokenize(text.lower())]
+
+vectorizer2 = TfidfVectorizer(stop_words = stop_words, tokenizer = tokenize)
+X2 = vectorizer2.fit_transform(desc)
+word_features2 = vectorizer2.get_feature_names()
+word_features2[:50]
+
+vectorizer3 = TfidfVectorizer(stop_words = stop_words, tokenizer = tokenize, max_features = 1000)
+X3 = vectorizer3.fit_transform(desc)
+words = vectorizer3.get_feature_names()
+
+kmeans = KMeans(n_clusters = 15, n_init = 5, n_jobs = -1)
+kmeans.fit(X3)
+
+common_words = kmeans.cluster_centers_.argsort()[:,-1:-11:-1]
+for num, centroid in enumerate(common_words):
+    print(str(num) + ' : ' + ', '.join(words[word] for word in centroid))
+
+variety_df['cluster'] = kmeans.labels_
+
+clusters = variety_df.groupby(['cluster', 'variety']).size()
+fig2, ax2 = plt.subplots(figsize = (30, 15))
+sns.heatmap(clusters.unstack(level = 'variety'), ax = ax2, cmap = 'Reds')
+
+ax2.set_xlabel('variety', fontdict = {'weight': 'bold', 'size': 24})
+ax2.set_ylabel('cluster', fontdict = {'weight': 'bold', 'size': 24})
+for label in ax2.get_xticklabels():
+    label.set_size(16)
+    label.set_weight("bold")
+for label in ax2.get_yticklabels():
+    label.set_size(16)
+    label.set_weight("bold")
+
+plt.show()
